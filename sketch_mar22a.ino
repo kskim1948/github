679d3ea6e349827de68226947b013e6b3fe878fe
kskim1948 <rudtjrdla@naver.com>
2017-03-23 23:05:58 +0900

    Add files via upload

--- sketch_mar22a.ino
+++ sketch_mar22a.ino
@@ -1,19 +1,27 @@
 const char* host = "api.thingspeak.com";
-String url = "/update?api_key=KI82SUY8GKTDG1OV";   // Your Own Key here
+String url = "/update?api_key=HYVDV2XNVPSI8CT2";   // Your Own Key here
 const int httpPort = 80;
 int interval = 60000;
-int r=5;
+
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
+#include <OneWire.h>
+
+#include <ESP8266WebServer.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS D4
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature DS18B20(&oneWire);
 
 const char* ssid = "iptime";   // Your own ssid here
 const char* password = "gpwjd1234";  // Your own password here
 
-String working() { 
-  
-  r = r+1; // do some magic here!
-  return(String("field1=")+String(r));
-}
+char temperatureString[6];
+String working(String data){ 
+  return (String("field1=")+String(data)); 
+} 
 
 void delivering(String payload) { 
   WiFiClient client;
@@ -52,18 +60,33 @@ void connect_ap() {
   Serial.println(WiFi.localIP());  
 }
 
+float getTemperature() {
+  float temp;
+  do {
+    DS18B20.requestTemperatures(); 
+    temp = DS18B20.getTempCByIndex(0);
+    delay(100);
+  } while (temp == 85.0 || temp == (-127.0));
+  return temp;
+}
+
 void setup() {
   Serial.begin(115200);
   connect_ap();
 
   Serial.println("ESPArduinoThingSpeak.cpp - 2017/3/15");
+
+   DS18B20.begin();
 }
 
-unsigned long mark = 0;
+
 void loop() {
-  if (millis() > mark ) {
-     mark = millis() + interval;
-     String payload = working();
-     delivering(payload);
-  }
-}
+  float temperature = getTemperature();
+
+  dtostrf(temperature, 2, 2, temperatureString);
+  // send temperature to the serial console
+  Serial.println(temperatureString);
+  
+    String payload = working(temperatureString);
+    delivering(payload);
+ }
